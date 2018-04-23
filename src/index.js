bb985b5f4d04c29ba7850a86f72ed55e1a8ea3e6
nathanielsiddall <nathaniel.siddall@gmail.com>
2018-04-23 16:55:45 -0500

    commiting for the day. it's not working. the index.ls wont change.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
 const $ = require('jquery');
-const sayHello =()=> {
-    return console.log("hello");
-};
-
+const sayHello =()=> console.log("hello");
 sayHello();
 
-$("#box").text("thing");
+$("#box").text("hello there");
 
 
-let  file = {
-    name: "bubba",
-    adress: "101 main",
-    id: "1234"
-}
 
 
 
